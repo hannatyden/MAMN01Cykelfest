9a78199ebcdeefb4b5bc19232ef0e6d893cd2d8f
Hanna Tydén <hannatyden@Hannas-MacBook-Pro.local>
2018-05-11 14:20:36 +0200

    lagt till info popup på hemskärmen

--- Cykelfest/app/src/main/java/com/example/hannatyden/cykelfest/MainMenu.java
+++ Cykelfest/app/src/main/java/com/example/hannatyden/cykelfest/MainMenu.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainMenu extends AppCompatActivity {
     CurrentParty currentParty;
@@ -13,6 +14,13 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        ImageButton ib = (ImageButton) findViewById(R.id.imageButton3);
+        ib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenu.this, Pop.class));
+            }
+        });
 
     }
 
