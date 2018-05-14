2413b43e48b6a9bd87d7c9b388e3738cec4d9ecb
Hanna Tydén <hannatyden@Hannas-MacBook-Pro.local>
2018-05-14 15:22:30 +0200

    fixat lista i hitta fest funktion

--- Cykelfest/app/src/main/java/com/example/hannatyden/cykelfest/FindParty.java
+++ Cykelfest/app/src/main/java/com/example/hannatyden/cykelfest/FindParty.java
@@ -3,54 +3,140 @@ package com.example.hannatyden.cykelfest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Debug;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FindParty extends AppCompatActivity {
-    //private static final String[]paths = {"Vinter", "Vår", "Sommar"};
-    //private ArrayList<String> toSend = new ArrayList<>();
-    //private ArrayList<String> parties = new ArrayList<>();
-    private ArrayList<PartyItem> parties = new ArrayList<>();
+    ListView l;
+    List<String> parties;
+    ArrayList<String> party;
+    ArrayList<PartyItem> partyItems;
+    ArrayAdapter<String> arrayAdapter;
+    boolean clickedButton;
+
+    /** Called when the user taps the Create party button */
+    public void createparty(View view) {
+        Intent intent = new Intent(this, CreateParty.class);
+        startActivityForResult(intent, 1);
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_party);
+        clickedButton = false;
 
-        //collect our intent
-        Intent intent = getIntent();
-        PartyItem partyItem = intent.getParcelableExtra("Party");
 
-        if(partyItem == null){
-            Log.i("test","Hallå");
+        l = findViewById(R.id.list);
+        parties = new ArrayList<String>();
+        System.out.println(parties);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, parties);
+        l.setAdapter(arrayAdapter);
+        if(partyItems == null) {
+            partyItems = new ArrayList<PartyItem>();
         }
-//        now collect all property values
-//        String theme = partyItem.getTheme();
-//        String name = partyItem.getName();
-//        String date = partyItem.getDate();
-//        String address = partyItem.getAddress();
-//        String max = partyItem.getMax();
-//        String endDate = partyItem.getEndDate();
 
-//        Bundle b = getIntent().getExtras();
-//        for(int i = 0; i < 6; i++){
-//            if (null != b) {
-//                String tema = b.getString("Tema");
-//                ArrayList<String> arrayOfParties = b.getStringArrayList("array_list");
-//
-//                Log.i("List", "Passed Array List :: " + arrayOfParties);
-//                System.out.print(arrayOfParties);
-//            }
-//        }
+        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) { // HÅRDKODAD ATT SKICKA ENBART EN STRÄNG FRÅN SENAST TILLAGDA NOTISEN. FUNKAR BARA FÖR 1 NOTIS
+            Intent intent = new Intent(getApplicationContext(), PartyInfoPopUp.class);
+            intent.putExtra("1", partyItems.get(position).name);
+            intent.putExtra("2", partyItems.get(position).theme);
+            intent.putExtra("3", partyItems.get(position).date);
+            intent.putExtra("4", partyItems.get(position).address);
+            intent.putExtra("5", partyItems.get(position).max);
+            intent.putExtra("6", partyItems.get(position).endDate);
+
+            startActivity(intent);
+        }
+        });
+
+        TextView tv = (TextView) findViewById(R.id.textview);
         
     }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+
+
+//        if(clickedButton) {
+//            Intent intent = new Intent();
+//            ArrayList<String> send = new ArrayList<String>();
+//            for (PartyItem p : partyItems) {
+//                send.add(p.name);
+//                send.add(p.theme);
+//                send.add(p.date);
+//                send.add(p.address);
+//                send.add(p.max);
+//                send.add(p.endDate);
+//            }
+//            intent.putStringArrayListExtra("partyArray2", send);
+//            setResult(RESULT_OK, intent);
+//
+//        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        System.out.println("BEFORE: ON-ACTIVITY-RESULT FOR party ACTIVITY");
+        if (requestCode == 1) {
+            System.out.println("ResultCode = " + resultCode + " Result OK: " + RESULT_OK);
+
+            if (resultCode == RESULT_OK) {
+                party = data.getStringArrayListExtra("partyArray");
+                PartyItem item = new PartyItem(party.get(0),party.get(1), party.get(2), party.get(3), party.get(4), party.get(5));
+                partyItems.add(item);
+                System.out.println("jag har tagit emot er lista");
+
+                for(PartyItem p : partyItems) {
+                    if(!parties.contains(p.name)){
+                        parties.add(p.name);
+                    }
+
+
+                }
+
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+        }
+    }
+
+    public void enterNotificationActivity(View view) {
+        Intent intent = new Intent(FindParty.this, CreateParty.class);
+        startActivityForResult(intent, 1);
+    }
+
+
 
 //    public void onActivityResult(int requestCode, int resultCode, Intent data){
 //        if(requestCode == 1){
@@ -64,25 +150,4 @@ public class FindParty extends AppCompatActivity {
 //        }
 //    }
 
-
-
-
-    //FindParty.UsersAdapter adapter = new FindParty.UsersAdapter(this, arrayOfParties);
-
-//    private static ArrayList<PartyItem> arrayOfParties = new ArrayList<>();
-//
-//    public static void addItem(PartyItem item) {
-//        arrayOfParties.add(item);
-//    }
-//
-//    public static ArrayList<PartyItem> getData() {
-//        return arrayOfParties;
-//    }
-
-
-//    public class UsersAdapter extends ArrayAdapter<PartyItem> {
-//        public UsersAdapter(Context context, ArrayList<PartyItem> party) {
-//            super(context, 0, party);
-//        }
-//    }
 }
--- Cykelfest/app/src/main/java/com/example/hannatyden/cykelfest/MainMenu.java
+++ Cykelfest/app/src/main/java/com/example/hannatyden/cykelfest/MainMenu.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.util.ArrayList;
+
 public class MainMenu extends AppCompatActivity {
     CurrentParty currentParty;
+    ArrayList<PartyItem> partyItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        partyItems = new ArrayList<PartyItem>();
+
 
         ImageButton ib = (ImageButton) findViewById(R.id.imageButton3);
         ib.setOnClickListener(new View.OnClickListener() {
@@ -36,17 +41,65 @@ public class MainMenu extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /** Called when the user taps the Create party button */
-    public void createparty(View view) {
-        Intent intent = new Intent(this, CreateParty.class);
-        startActivity(intent);
-    }
+
 
    /** Called when the user taps the find party button */
     public void findparty(View view) {
         Intent intent = new Intent(this, FindParty.class);
         startActivity(intent);
     }
+
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        System.out.println("Halloj");
+//        if (requestCode == 1) {
+//            System.out.println("ResultCode = " + resultCode + " Result OK: " + RESULT_OK);
+//
+//            if (resultCode == RESULT_OK) {
+//                ArrayList<String> recieved = data.getStringArrayListExtra("partyArray2");
+//                //toSend.add(party);
+//                int index = 0;
+//                PartyItem item = new PartyItem("1","1","1","1","1","1");
+//                for(String s : recieved) {
+//                    if(index == 5) {
+//                        index = 0;
+//                        partyItems.add(item);
+//                        item = new PartyItem("1","1","1","1","1","1");
+//
+//                    }
+//
+//                    switch (index) {
+//                        case 0:
+//                            item.setName(s);
+//                            break;
+//                        case 1:
+//                            item.setTheme(s);
+//                            break;
+//                        case 2:
+//                            item.setDate(s);
+//                            break;
+//                        case 3:
+//                            item.setAddress(s);
+//                            break;
+//                        case 4:
+//                            item.setMax(s);
+//                            break;
+//                        case 5:
+//                            item.setEndDate(s);
+//                            break;
+//                    }
+//                    index++;
+//
+//
+//                }
+//
+//                System.out.println("jag har tagit emot er lista menyn");
+//
+//
+//             //   arrayAdapter.notifyDataSetChanged();
+//
+//            }
+//
+//        }}
 
 
 }
